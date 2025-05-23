368722d32970360f7949a952d6af20797b0fd50b
Vugar <vugar.khanguliyev@gmail.com>
2025-05-23 20:53:07 +0400

    Created black screen

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+    ASTEROID_MAX_RADIUS,
+)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
